b875b8129514f821068ef3f677dfe2c8fd2b624c
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-03-20 19:42:38 +0600

    All Done

--- src/fakeData/transportData.js
+++ src/fakeData/transportData.js
@@ -1,7 +1,7 @@
 const transports = [
   { name: "Bike", image: "https://cdn4.iconfinder.com/data/icons/transportation-volume-ii/128/motorcycle-3-512.png"},
-  { name: "Car", image: "https://www.flaticon.com/svg/vstatic/svg/3089/3089803.svg?token=exp=1616241402~hmac=595c3cf0d052e3c89cf5161742e80d94"},
-  { name: "Bus", image: "https://www.flaticon.com/svg/vstatic/svg/744/744466.svg?token=exp=1616241492~hmac=9c6aa545117c1e04e57182dfa6766c28"},
-  { name: "Train", image: "https://www.flaticon.com/svg/vstatic/svg/713/713309.svg?token=exp=1616241595~hmac=4dcdad7463afc326895502f12f48a95c"},
+  { name: "Car", image: "https://cdn0.iconfinder.com/data/icons/isometric-city-basic-transport/480/car-taxi-front-01-512.png"},
+  { name: "Bus", image: "https://cdn4.iconfinder.com/data/icons/Travel_Icon_Pack/PNG/bus.png"},
+  { name: "Train", image: "https://cdn1.iconfinder.com/data/icons/railroad-cartoon/512/g3177-512.png"},
 ]
 export default transports;
