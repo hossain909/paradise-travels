7a56ea0869adeccb35522af824cf6a7cb5687903
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-03-20 19:34:34 +0600

    All Done

--- src/fakeData/transportData.js
+++ src/fakeData/transportData.js
@@ -1,5 +1,5 @@
 const transports = [
-  { name: "Bike", image: "https://www.flaticon.com/svg/vstatic/svg/3148/3148937.svg?token=exp=1616241469~hmac=333da6ee3413e67dacf2def0a806eb53"},
+  { name: "Bike", image: "https://cdn4.iconfinder.com/data/icons/transportation-volume-ii/128/motorcycle-3-512.png"},
   { name: "Car", image: "https://www.flaticon.com/svg/vstatic/svg/3089/3089803.svg?token=exp=1616241402~hmac=595c3cf0d052e3c89cf5161742e80d94"},
   { name: "Bus", image: "https://www.flaticon.com/svg/vstatic/svg/744/744466.svg?token=exp=1616241492~hmac=9c6aa545117c1e04e57182dfa6766c28"},
   { name: "Train", image: "https://www.flaticon.com/svg/vstatic/svg/713/713309.svg?token=exp=1616241595~hmac=4dcdad7463afc326895502f12f48a95c"},
